84ab5dec594cf868deec96df04a8199cd9a1a2b9
Raine Edoria <edoria.419982@balagtas.sti.edu.ph>
2026-02-10 19:57:02 +0800

    Adding Subtraction Function Add Subtraction class for subtraction operations A new `Subtraction.cs` file has been added, containing an `internal` `Subtraction` class with a `Subtract` method that computes the difference between two `double` values. The `Sulit_GitHub_Calculator_Sample.csproj` file has been updated to include the `Subtraction.cs` file, ensuring it is compiled and part of the project build.

---
+++ Sulit_GitHub_Calculator_Sample/Subtraction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sulit_GitHub_Calculator_Sample
+{
+    internal class Subtraction
+    {
+        public double Subtract(double num1, double num2) // Subtraction method that takes two double parameters and returns their difference
+        {
+            return num1 - num2; // Returns the result of subtracting num2 from num1
+        }
+    }
+}
